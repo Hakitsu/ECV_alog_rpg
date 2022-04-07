c169289770f40ba3a1a80d35723b2e7f6d31ae60
Lea Renaux <lea.renaux@mail-ecv.fr>
2022-04-07 16:46:40 +0200

    add class guerrier & mage

---
+++ classGuerrier.js
@@ -0,0 +1,48 @@
+class Guerrier {
+    
+    constructor(selector) {
+    this.container = document.querySelector(selector);
+    this.lifeMax = 100;
+    this.life = 100;
+    this.defense = 100;
+    this.damageCaC = 50;
+    this.experience = 0;
+    this.lvl = 1;
+    this.experienceMax = 50;
+    this.isAlive = true;
+    }
+
+    set setExperience(exp){  // quand il tue un monstre
+        if(this.experienceMax < this.experience + exp){
+            this.lvl = this.lvl +1;
+            this.experienceMax = this.experienceMax + 50;
+            this.experience = (this.experience + exp ) - this.experienceMax;
+        }
+        else{
+            this.experience = this.experience + exp;
+        }
+    }
+    
+    attack() {  // quand il attaque
+        return this.damageCaC
+    }
+
+    touchByAttack(damageRecieved){  // quand il est attaqué
+        this.life = this.life - damageRecieved
+
+        if(this.life < 0 ){
+            this.isAlive = false;
+        }
+
+    }
+
+    heal() {  // quand il se soigne
+        if(this.lifeMax < this.life + 100){
+            this.life = this.lifeMax;
+        }
+        else{
+            this.life = this.life + 100 ;
+        }
+    }   
+    
+}
