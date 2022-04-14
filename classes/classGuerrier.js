29f909937c6dc7dd886145bd46812c3b772c1571
Lea Renaux <lea.renaux@mail-ecv.fr>
2022-04-14 16:15:58 +0200

    ajout de potion et argent sur guerrier

--- classes/classGuerrier.js
+++ classes/classGuerrier.js
@@ -11,14 +11,32 @@ class Guerrier {
     this.lvl = 1;
     this.experienceMax = 50;
     this.isAlive = true;
+    this.gold = 100;
+    this.nbPotion = 0;
     }
 
-    setExperience(exp){  // quand il tue un monstre
+    setExperience(exp, gold){  // quand il tue un monstre
+        this.gold = this.gold + gold; 
+        console.log("Argent : " + this.gold)
         if(this.experienceMax < this.experience + exp){
             this.lvl = this.lvl +1;
             this.experience = (this.experience + exp ) - this.experienceMax;
             this.experienceMax = this.experienceMax + 50;
             console.log("Le guerrier est monté au niveau " , this.lvl);
+            
+            let action =  window.prompt("Voulez-vous achetez une potion pour 100 golds ? (oui / non) ")
+            if(action === "oui"){
+                if(this.gold >= 100){
+                    this.gold = this.gold - 100;
+                    this.nbPotion = this.nbPotion + 1;
+                    console.log("vous avez maintenant : " + this.nbPotion + " potions et " + this.gold + " argents")
+                }
+                else {
+                    alert("Vous n'avez pas assez d'argent !")
+                }
+            }
+
+
         }
         else{
             this.experience = this.experience + exp;
@@ -33,7 +51,6 @@ class Guerrier {
         if(probaEchec === 5){
             console.log("Guerrier a attaqué le monstre ! Il a raté son attaque ");
             return 0;
-
         }
         console.log("Guerrier a attaqué le monstre ! Le monstre a subit ", this.attackCaC , " dégats ")
         return this.attackCaC
@@ -52,14 +69,20 @@ class Guerrier {
     }
 
     heal() {  // quand il se soigne
-        if(this.lifeMax < this.life + 100){
-            this.life = this.lifeMax;
+        if(this.nbPotion > 0){
+            if(this.lifeMax < this.life + 100){
+                this.life = this.lifeMax;
+            }
+            else{
+                this.life = this.life + 100 ;
+            }
+            this.nbPotion = this.nbPotion - 1;
+            console.log("Guerrier s'est soigné, ses PV sont maintentant de   " , this.life);
         }
-        else{
-            this.life = this.life + 100 ;
+        else {
+            console.log("Guerrier ne peut pas se soigner, il n'a plus de potion !")
         }
 
-        console.log("Guerrier s'est soigné, ses PV sont maintentant de   " , this.life);
 
     }
 }
